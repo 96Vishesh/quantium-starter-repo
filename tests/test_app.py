da3a38f3e6b98fbfb5c2b70b20ae3dc86702f6e8
Vishesh96 <srivastava.vishesh96@gmail.com>
2026-02-10 19:20:41 +0530

    Completed Quantium Dash project with tests

---
+++ tests/test_app.py
@@ -0,0 +1,26 @@
+import sys
+import os
+
+# Fix import path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app
+
+
+def test_header_present(dash_duo):
+    dash_duo.start_server(app)
+    header = dash_duo.find_element("h1")
+    assert header is not None
+    assert "Soul Foods" in header.text
+
+
+def test_graph_present(dash_duo):
+    dash_duo.start_server(app)
+    graph = dash_duo.find_element("#sales-chart")
+    assert graph is not None
+
+
+def test_region_picker_present(dash_duo):
+    dash_duo.start_server(app)
+    radio = dash_duo.find_element("#region-filter")
+    assert radio is not None
